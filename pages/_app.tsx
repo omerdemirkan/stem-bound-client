ebf6a3705e5002b217a10a18382c080ea0fc06f9
omerdemirkan <omerfarukpiano@gmail.com>
2020-06-12 17:48:43 -0700

    set up redux with empty auth reducer and nextjs wrapper

---
+++ pages/_app.tsx
@@ -0,0 +1,14 @@
+import { Provider } from 'react-redux';
+import store from '../store';
+import { createWrapper } from 'next-redux-wrapper';
+
+const App: React.FC = function({ Component, pageProps }: any) {
+    
+    return <Provider store={store}>
+        <Component  {...pageProps}></Component>
+    </Provider>
+}
+
+const wrapper = createWrapper(() => store);
+
+export default wrapper.withRedux(App);
