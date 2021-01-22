7b12962830e2fca7997bb54a46daa6e2ec1da97c
Omer Demirkan <omerfarukpiano@gmail.com>
2021-01-22 14:37:33 -0800

    fix legacy (redux) create course page imports

--- src/pages/app/courses/create.tsx
+++ src/pages/app/courses/create.tsx
@@ -1,8 +1,8 @@
 import withAuth from "../../../components/hoc/withAuth";
 import AppLayout from "../../../components/containers/AppLayout";
 import Head from "next/head";
-import { EUserRoles, EStateStatus } from "../../../utils/types";
-import { useState, useContext } from "react";
+import { EUserRoles } from "../../../utils/types";
+import { useContext } from "react";
 import { useRouter } from "next/router";
 import { createCourse } from "../../../utils/services";
 import AuthContext from "../../../components/contexts/AuthContext";
@@ -11,17 +11,11 @@ import CourseForm from "../../../components/forms/CourseForm";
 const CreateCourseAppPage: React.FC = () => {
     const router = useRouter();
     const { user } = useContext(AuthContext);
-    const [status, setStatus] = useState<EStateStatus>(EStateStatus.idle);
 
     function handleSubmit(values) {
-        createCourse(values)
-            .then(function (res) {
-                setStatus(EStateStatus.successful);
-                router.push("/app/courses");
-            })
-            .catch(function (err) {
-                setStatus(EStateStatus.failed);
-            });
+        createCourse(values).then(function (res) {
+            router.push("/app/courses");
+        });
     }
 
     return (
@@ -34,7 +28,6 @@ const CreateCourseAppPage: React.FC = () => {
             <Head>
                 <title>STEM-bound - Create Course</title>
             </Head>
-            {status === EStateStatus.loading ? <h3>Loading...</h3> : null}
             <CourseForm onSubmit={handleSubmit} userId={user._id} />
         </AppLayout>
     );
