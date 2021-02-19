f51b61c8eb3eabf5e59d6dfe60272138a5d0f514
Omer Demirkan <omerfarukpiano@gmail.com>
2021-02-19 10:54:56 -0800

    create dynamic logo component

---
+++ src/components/ui/Logo.tsx
@@ -0,0 +1,50 @@
+import { ETheme } from "../../utils/types";
+import WordLogoSVG from "../svg/icons/word-logo";
+import WordLogoDarkModeSVG from "../svg/icons/word-logo-dark-mode";
+import LogoSVG from "../svg/icons/logo";
+import Typography from "@material-ui/core/Typography";
+import Box, { BoxProps } from "@material-ui/core/Box";
+import { makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles({
+    beta: {
+        lineHeight: 0,
+        fontSize: ".8rem",
+        fontWeight: 500,
+        textAlign: "end",
+        opacity: 0.6,
+        position: "relative",
+        bottom: "6px",
+    },
+});
+
+export interface ILogoProps extends BoxProps {
+    theme?: ETheme;
+    type?: "word" | "icon";
+    beta?: boolean;
+    SvgProps?: React.SVGProps<SVGSVGElement>;
+}
+
+const Logo: React.FC<ILogoProps> = ({
+    theme,
+    type,
+    beta,
+    SvgProps,
+    ...boxProps
+}) => {
+    const classes = useStyles();
+    const SvgComponent =
+        type === "word"
+            ? theme === ETheme.DARK
+                ? WordLogoDarkModeSVG
+                : WordLogoSVG
+            : LogoSVG;
+    return (
+        <Box component="div" {...boxProps}>
+            <SvgComponent width="100%" height="auto" {...SvgProps} />
+            {beta && <Typography className={classes.beta}>BETA</Typography>}
+        </Box>
+    );
+};
+
+export default Logo;
