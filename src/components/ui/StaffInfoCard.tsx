99be9cadb91f18d5546aa8a0cf2677b5207667d5
Mehmet Nadi <mhmtalperennadi@gmail.com>
2021-03-15 15:54:35 -0700

    our team page finish

---
+++ src/components/ui/StaffInfoCard.tsx
@@ -0,0 +1,113 @@
+import useMediaQuery from "@material-ui/core/useMediaQuery";
+import Typography from "@material-ui/core/Typography";
+import Box from "@material-ui/core/Box";
+import Button from "@material-ui/core/Button";
+import { makeStyles } from "@material-ui/core";
+import Link from "next/link";
+
+const useStyles = makeStyles({
+    highlightButton: {
+        margin: "4px 0",
+        width: "40%",
+    },
+});
+
+const TeamCard = (props) => {
+    const mobileSize = useMediaQuery("(max-width: 900px)");
+    const classes = useStyles();
+
+    return (
+        <>
+            <div className="staff-info-card-wrapper">
+                <div className="img-wrapper">
+                    <div className="person-contact-container">
+                        <a href="mailto:someone@yoursite.com" target="_blank">
+                            <img
+                                src="email-icon.svg"
+                                alt="email-icon"
+                                className="contact-icon"
+                            />
+                        </a>
+                        <a href={props.gitHub} target="_blank">
+                            <img
+                                src="github-icon.svg"
+                                alt="github-icon"
+                                className="contact-icon"
+                            />
+                        </a>
+                        <a href={props.linkIn} target="_blank">
+                            <img
+                                src="linkedin-icon.svg"
+                                alt="linkedn-icon"
+                                className="contact-icon"
+                            />
+                        </a>
+                    </div>
+                </div>
+                <Typography variant="h2" component="h1" color="primary">
+                    <Box fontSize={30} fontWeight={500} lineHeight={1.5}>
+                        {props.name}
+                    </Box>
+                </Typography>
+                <Typography variant="h2" component="h1" color="primary">
+                    <Box fontSize={20} lineHeight={1.5}>
+                        <p
+                            style={{
+                                textDecorationLine: "underline",
+                                margin: "0",
+                            }}
+                        >
+                            {props.position}
+                        </p>
+                    </Box>
+                </Typography>
+                <Typography variant="body1" component="p">
+                    <Box fontSize={20} lineHeight={1.5}>
+                        {props.bio}
+                    </Box>
+                </Typography>
+            </div>
+            <style jsx>{`
+                .staff-info-card-wrapper {
+                    min-width: 300px;
+                    max-width: 30%;
+                    margin: 5% auto;
+                    text-align: left;
+                }
+
+                .img-wrapper {
+                    background-image: url(${`${props.imgUrl}`});
+                    background-size: cover;
+                    width: 250px;
+                    height: 250px;
+                    align-items: center;
+                    text-align: center;
+                }
+
+                .person-contact-container {
+                    height: 100%;
+                    display: flex;
+                    align-items: center;
+                    text-align: center;
+                    justify-content: space-evenly;
+                    background: rgba(157, 141, 254, 0.74);
+                    border-radius: 25px;
+                }
+
+                .contact-icon {
+                    margin: 5px;
+                    width: 40px;
+                    height: 40px;
+                }
+                .contact-icon:hover {
+                    cursor: pointer;
+                    width: 45px;
+                    height: 45px;
+                }
+            `}</style>
+            );
+        </>
+    );
+};
+
+export default TeamCard;
--- src/pages/our-team.tsx
+++ src/pages/our-team.tsx
@@ -1,15 +1,96 @@
-import StaticLayout from "../components/ui/StaticLayout"
-
-
-
-
-
+import StaticLayout from "../components/ui/StaticLayout";
+import StaffInfoCard from "../components/ui/StaffInfoCard";
 
 const OurTeamPage = () => {
-    return <StaticLayout>
-        <h1>you are not dumb</h1>
-    </StaticLayout>
-}
+    const teamMember = [
+        {
+            id: 0,
+            name: "Omer Demirkan",
+            position: "CEO & Founder",
+            bio:
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+            imgUrl: "Omer-pp.png",
+            email: "omerfarukpiano@gmail.com",
+            gitHub: "https://github.com/omerdemirkan",
+            linkIn: "https://www.linkedin.com/in/omer-demirkan",
+        },
+        {
+            id: 1,
+            name: "Mehmet Nadi",
+            position: "Unknown positon",
+            bio:
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+            imgUrl: "Mehmet-pp.png",
+            email: "mhmtalperennadi@gmail.com",
+            gitHub: "https://github.com/mehmetalperen",
+            linkIn: "https://www.linkedin.com/in/mehmet-nadi-03372a1b1",
+        },
+        {
+            id: 2,
+            name: "Emmad Usmani",
+            position: "Unknown positon",
+            bio:
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+            imgUrl: "Emmad-pp.png",
+            email: "emmadusmani@berkeley.edu",
+            gitHub: "https://github.com/EmmadUsmani",
+            linkIn: "https://www.linkedin.com/in/emmadusmani/",
+        },
+    ];
+    return (
+        <StaticLayout>
+            <div className="our-team-page-wrapper">
+                <div className="page-title-container">
+                    <h1 className="page-title">Meet Our Team</h1>
+                </div>
 
+                <div className="team-section-wrapper">
+                    {teamMember.map((member) => {
+                        return (
+                            <StaffInfoCard
+                                key={member.id}
+                                name={member.name}
+                                position={member.position}
+                                bio={member.bio}
+                                imgUrl={member.imgUrl}
+                                email={member.email}
+                                gitHub={member.gitHub}
+                                linkIn={member.linkIn}
+                            />
+                        );
+                    })}
+                </div>
+            </div>
+
+            <style jsx>{`
+                .our-team-page-wrapper {
+                    min-height: 100vh;
+                    width: 100%;
+                }
+
+                .page-title-container {
+                    max-width: 400px;
+                    margin: 30px auto;
+                }
+                .page-title {
+                    font-style: normal;
+                    font-weight: 300;
+                    font-size: 50px;
+                    margin: 0;
+                    text-align: center;
+                    color: #826efd;
+                }
+
+                .team-section-wrapper {
+                    display: flex;
+                    flex-wrap: wrap;
+                    margin: 0 auto;
+                    max-width: 90%;
+                    justify-content: space-between;
+                }
+            `}</style>
+        </StaticLayout>
+    );
+};
 
 export default OurTeamPage;
