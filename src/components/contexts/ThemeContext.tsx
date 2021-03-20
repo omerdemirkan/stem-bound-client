9dfc323f6f8f7d54ad0663e14c2beed5df0d87c9
omerdemirkan <omerfarukpiano@gmail.com>
2021-03-19 20:33:27 -0700

    add (possibly) temporary static page dark mode error fix

--- src/components/contexts/ThemeContext.tsx
+++ src/components/contexts/ThemeContext.tsx
@@ -1,4 +1,5 @@
 import { MuiThemeProvider } from "@material-ui/core/styles";
+import { useRouter } from "next/router";
 import { createContext, useState } from "react";
 import { darkTheme, lightTheme } from "../../utils/constants";
 import { getTheme } from "../../utils/services";
@@ -14,20 +15,27 @@ const ThemeContext = createContext(initialThemeContextState);
 export default ThemeContext;
 
 export const ThemeProvider: React.FC = ({ children }) => {
+    const router = useRouter();
     const [theme, setTheme] = useState<ETheme>(getTheme());
+
     function handleChangeTheme(theme: ETheme) {
         setTheme(theme);
         localStorage.setItem("theme", theme);
     }
+
+    const displayTheme = router?.pathname?.includes("/app")
+        ? theme
+        : ETheme.LIGHT;
+
     return (
         <ThemeContext.Provider
             value={{
-                theme,
+                theme: displayTheme,
                 setTheme: handleChangeTheme,
             }}
         >
             <MuiThemeProvider
-                theme={theme === ETheme.LIGHT ? lightTheme : darkTheme}
+                theme={displayTheme === ETheme.LIGHT ? lightTheme : darkTheme}
             >
                 {children}
             </MuiThemeProvider>
