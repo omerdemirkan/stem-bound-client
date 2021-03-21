1a110f094c37a16e39eb9ed0bde4504b2810b425
omerdemirkan <omerfarukpiano@gmail.com>
2021-03-21 12:43:10 -0700

    create custom copy to clipboard component

---
+++ src/components/util/CopyToClipboard.tsx
@@ -0,0 +1,73 @@
+import Box from "@material-ui/core/Box";
+import Tooltip from "@material-ui/core/Tooltip";
+import FileCopyIcon from "@material-ui/icons/FileCopy";
+import { useContext } from "react";
+import NotificationContext from "../contexts/NotificationContext";
+
+import IconButton from "@material-ui/core/IconButton";
+import Typography from "@material-ui/core/Typography";
+
+export interface ICopyToClipboardProps {
+    text: string;
+    description: string;
+    onCopy?(): any;
+}
+
+const CopyToClipboard: React.FC<ICopyToClipboardProps> = ({
+    text,
+    description,
+    onCopy,
+}) => {
+    const { createSnackbar } = useContext(NotificationContext);
+    async function handleCopyToClipboard() {
+        try {
+            await navigator.clipboard.writeText(text);
+            createSnackbar({
+                text: `${description} copied to clipboard`,
+                type: "info",
+            });
+            onCopy?.();
+        } catch (e) {
+            createSnackbar({
+                text: "Couldn't copy to clipboard",
+                type: "error",
+            });
+        }
+    }
+
+    return (
+        <div className="container">
+            <Typography component="span" noWrap style={{ maxWidth: "400px" }}>
+                {text}
+            </Typography>
+            <span>
+                <Tooltip title={`Copy ${description}`}>
+                    <IconButton
+                        color="primary"
+                        size="small"
+                        onClick={handleCopyToClipboard}
+                    >
+                        <FileCopyIcon />
+                    </IconButton>
+                </Tooltip>
+            </span>
+
+            <style jsx>{`
+                .container {
+                    background-color: var(--background-light);
+                    border-radius: 4px;
+                    padding: 3px 8px;
+                    width: 100%;
+                    max-width: 450px;
+
+                    display: grid;
+                    grid-template-columns: auto 30px;
+                    grid-gap: 10px;
+                    align-items: center;
+                }
+            `}</style>
+        </div>
+    );
+};
+
+export default CopyToClipboard;
