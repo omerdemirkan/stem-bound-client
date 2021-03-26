f2682594a1c13160c8ba2dfee609eea64f6574be
Mehmet Nadi <mhmtalperennadi@gmail.com>
2021-03-25 17:17:49 -0700

    add shapes to the homePage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,11 +10,50 @@ const HomePage: React.FC = function () {
             <Head>
                 <title>STEM-bound - Home</title>
             </Head>
+            <img src="triangle.svg" id="triangle" className="shape" alt="" />
+            <img
+                src="circleLeft.svg"
+                id="circle-left"
+                className="shape"
+                alt=""
+            />
             <HomePageIntro />
+            <img src="square.svg" alt="" id="square" className="shape" />
             <HomePageInfo />
+            <img
+                src="circleRight.svg"
+                alt=""
+                id="circle-right"
+                className="shape"
+            />
             <HomePageAppDetail />
 
-            <style jsx>{``}</style>
+            <style jsx>{`
+                .shape {
+                    position: absolute;
+                    z-index: -1;
+                }
+
+                #triangle {
+                    top: 0;
+                    left: 20%;
+                }
+                #circle-left {
+                    top: 150px;
+                }
+                #square {
+                    top: 100vh;
+                }
+                #circle-right {
+                    right: 0;
+                }
+
+                @media (max-width: 900px) {
+                    .shape {
+                        display: none;
+                    }
+                }
+            `}</style>
         </StaticLayout>
     );
 };
