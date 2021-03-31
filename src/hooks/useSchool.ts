5f830a305b6e08bbeedd9d46adc57397d6f5b33e
omerdemirkan <omerfarukpiano@gmail.com>
2021-03-31 11:46:51 -0700

    create simple useSchool hook

---
+++ src/hooks/useSchool.ts
@@ -0,0 +1,13 @@
+import { schoolFetcher } from "../utils/services";
+import { ISchool } from "../utils/types";
+import { useFetchOnce } from "./useFetchOnce";
+
+export function useSchool(
+    ncesId: string
+): { school: ISchool; loading: boolean } {
+    const { data: school, isValidating: loading } = useFetchOnce(
+        ncesId ? `/school/${ncesId}` : null,
+        schoolFetcher(ncesId)
+    );
+    return { school, loading };
+}
